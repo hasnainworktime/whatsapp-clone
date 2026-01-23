4613ebd1e60e09cf4dbecd73f421328bb54d9250
hasnainworktime <hasnain.workday@gmail.com>
2026-01-23 19:01:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// JavaScript will come later
+const sendBtn = document.querySelector("button");
+const input = document.querySelector("input");
+const messages = document.querySelector(".messages");
+
+sendBtn.addEventListener("click", sendMessage);
+
+function sendMessage() {
+  const text = input.value.trim();
+
+  if (text === "") return;
+
+  const msg = document.createElement("div");
+  msg.classList.add("message", "sent");
+  msg.innerText = text;
+
+  messages.appendChild(msg);
+  input.value = "";
+
+  messages.scrollTop = messages.scrollHeight;
+}
